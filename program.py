3944777688c9493c6bb12da19eea22026874176b
Dean Chapman <dean.chapman@kroo.com>
2024-08-15 18:27:19 +0200

    First question done

--- program.py
+++ program.py
@@ -1,5 +1,5 @@
 def badrange_check(to_check):
-    if len(to_check) > 15 or len(to_check) < 5 or  to_check == '':
+    if len(to_check) > 15 or len(to_check) < 5 or to_check == None:
         return True
 
 def user_is_authenticated(username,password):
@@ -26,4 +26,24 @@ while not authenticated and attempts < 3:
         attempts = attempts + 1
 
 if authenticated:
-    print('Welcome to the system')
+    print('Welcome to the Music Quiz!')
+    print('Here\'s how to play')
+
+    score=0
+    answer1=False
+    questionattempts=0
+    while answer1==False and questionattempts <2:
+        print('the artist for round one is taylor swift')
+        song=input('the song initials are s---- i- o--')
+
+        if song.lower() == 'shake it off':
+            print('correct')
+            answer1=True
+            score=score + 3 if questionattempts == 0 else 1
+        else:    
+            print('incorrect')
+            questionattempts = questionattempts + 1
+            if questionattempts == 2:
+                print('no more attempts')
+                exit()
+
